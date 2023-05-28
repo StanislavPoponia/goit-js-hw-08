d7a0345d17aa9ef938af9b4d25f677ba165e5ea7
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-05-28 15:06:28 +0300

    try 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryElement = document.querySelector('.gallery');
+
+const imagesElement = galleryItems
+  .map(({original,preview,description })  =>
+      `<li>
+      <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></li>`
+  )
+  .join('');
+
+galleryElement.insertAdjacentHTML('afterbegin', imagesElement);
+
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function onPlay({ seconds }) {
+  localStorage.setItem('video-current-time', JSON.stringify(seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 800));
+
+let seconds = JSON.parse(localStorage.getItem('video-current-time'));
+if (seconds) {
+  player.setCurrentTime(seconds);
+}
