a7e52c5dc591f1974a5a6035b39691ea6b005d26
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-05-29 11:00:14 +0300

    update second js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,16 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const playVideo = new Vimeo(iframe);
+const player = new Vimeo(iframe);
 
-playVideo.on('timeupdate', throttle(onSaveTimePlayer, 800));
+player.on('timeupdate', throttle(saveTimeOnPlayer, 800));
 
-function onSaveTimePlayer(event) {
+function saveTimeOnPlayer(event) {
   const playerTime = event.seconds;
   localStorage.setItem(STORAGE_KEY, playerTime);
 }
-const saveTimeVideo = localStorage.getItem(STORAGE_KEY);
+const saveTimeStorage = localStorage.getItem(STORAGE_KEY);
 
-if (saveTimeVideo) {
-  playVideo.setCurrentTime(saveTimeVideo);
+if (saveTimeStorage) {
+  player.setCurrentTime(saveTimeStorage);
 }
